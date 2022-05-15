145ec2cbdcec21466d796938b7fd3035542b42a3
RacheliCh <racheli.ch@campus.technion.ac.il>
2022-05-15 17:10:43 +0300

    gui: changed font size and place

--- main_app.py
+++ main_app.py
@@ -13,31 +13,31 @@ my_camera = cameraInitialize()
 # gui layouts
 sg.theme('LightPurple')
 layout_take_picture = [
-    [sg.Button('Take Picture')],
-    [sg.Exit()]
+    [sg.Button('Take Picture', size=(20,2), font=("calibri", 58))],
+    [sg.Exit(size=(20,2), font=("calibri", 58))]
 ]
 layout_fields = [
-    [sg.Text('Type', size=(15,1)), 
-        sg.Radio('SHIRT', "RADIO_TYPE", default=False, key='shirt'), 
-        sg.Radio('SKIRT', "RADIO_TYPE", default=False, key='skirt'), 
-        sg.Radio('PANTS', "RADIO_TYPE", default=False, key='pants')],
-    [sg.Text('Size', size=(15,1)), 
-        sg.Radio('S', "RADIO_SIZE", default=False, key='s'), 
-        sg.Radio('M', "RADIO_SIZE", default=False, key='m'), 
-        sg.Radio('L', "RADIO_SIZE", default=False, key='l')],
-    [sg.Text('Color', size=(15,1)), 
-        sg.Radio('RED', "RADIO_COLOR", default=False, key='red'), 
-        sg.Radio('GREEN', "RADIO_COLOR", default=False, key='green'), 
-        sg.Radio('BLUE', "RADIO_COLOR", default=False, key='blue')],
-    [sg.Text('Price', size=(15,1)), 
-        sg.Radio('10', "RADIO_PRICE", default=False, key='10'), 
-        sg.Radio('20', "RADIO_PRICE", default=False, key='20'), 
-        sg.Radio('30', "RADIO_PRICE", default=False, key='30')],
-    [sg.Submit(), sg.Button('Clear'), sg.Exit()]
+    [sg.Text('Type', size=(15,1),font=("calibri", 20)), 
+        sg.Radio('SHIRT', "RADIO_TYPE", default=False, key='shirt',font=("calibri", 20)), 
+        sg.Radio('SKIRT', "RADIO_TYPE", default=False, key='skirt',font=("calibri", 20)), 
+        sg.Radio('PANTS', "RADIO_TYPE", default=False, key='pants',font=("calibri", 20))],
+    [sg.Text('Size', size=(15,1),font=("calibri", 20)), 
+        sg.Radio('S', "RADIO_SIZE", default=False, key='s',font=("calibri", 20)), 
+        sg.Radio('M', "RADIO_SIZE", default=False, key='m',font=("calibri", 20)), 
+        sg.Radio('L', "RADIO_SIZE", default=False, key='l',font=("calibri", 20))],
+    [sg.Text('Color', size=(15,1),font=("calibri", 20)), 
+        sg.Radio('RED', "RADIO_COLOR", default=False, key='red',font=("calibri", 20)), 
+        sg.Radio('GREEN', "RADIO_COLOR", default=False, key='green',font=("calibri", 20)), 
+        sg.Radio('BLUE', "RADIO_COLOR", default=False, key='blue',font=("calibri", 20))],
+    [sg.Text('Price', size=(15,1),font=("calibri", 20)), 
+        sg.Radio('10', "RADIO_PRICE", default=False, key='10',font=("calibri", 20)), 
+        sg.Radio('20', "RADIO_PRICE", default=False, key='20',font=("calibri", 20)), 
+        sg.Radio('30', "RADIO_PRICE", default=False, key='30',font=("calibri", 20))],
+    [sg.Submit(font=("calibri", 20)), sg.Button('Clear',font=("calibri", 20)), sg.Exit(font=("calibri", 20))]
 ]
 
-window_take_picture = sg.Window('Take Picture', layout_take_picture)
-window_fields = sg.Window('Select Info Fields', layout_fields)
+window_take_picture = sg.Window('Take Picture', layout_take_picture, size=(800, 480), element_justification='c')
+window_fields = sg.Window('Select Info Fields', layout_fields, size=(800, 480))
 
 # gui functions
 def gui_clear_input():
