93c01f26471e7b18b876cfbcb9f182a5c622e3e4
RacheliCh <racheli.ch@campus.technion.ac.il>
2022-06-12 11:42:13 +0300

    fix in create_executable script

--- create_executable.sh
+++ create_executable.sh
@@ -2,9 +2,9 @@
 #    from the python files and the item_config.yaml file
 # the executable file will run automatically when rpi turnes on 
 #    and when you run the alias "app" on the terminal
-cd /home/pi/Documents/project/Donate-It
-pyinstaller main_app.py 
-cp dist/main_app/main_app /home/pi/Documents/project/Donate-It
+
+pyinstaller --onefile --noconsole /home/pi/Documents/project/Donate-It/main_app.py 
+cp /home/pi/Documents/project/Donate-It/dist/main_app /home/pi/Documents/project/Donate-It
 rm -r /home/pi/Documents/project/Donate-It/build
 rm -r /home/pi/Documents/project/Donate-It/dist
 rm /home/pi/Documents/project/Donate-It/main_app.spec
